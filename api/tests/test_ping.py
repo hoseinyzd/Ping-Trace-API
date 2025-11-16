90ae16ed9ab707dc3507055717f21643db20fee7
HosseinYZD <hoseinyzdp@gmail.com>
2025-11-16 10:24:07 +0330

    Add pytest + CI workflow + httpx fix

--- api/tests/test_ping.py
+++ api/tests/test_ping.py
@@ -1,5 +1,11 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+
 from fastapi.testclient import TestClient
 from api.main import app
+
+
 
 client = TestClient(app)
 
